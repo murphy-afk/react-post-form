e1d62ea654d4b944cfaaa0e6315cafdde7f5d0c5
murphy-afk <bardarenoemi@gmail.com>
2025-12-04 15:26:36 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>test</h1>
+    </>
+  )
+}
+
+export default App

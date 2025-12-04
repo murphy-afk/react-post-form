5a1460489264e66cae8aeb55a3a35de78e43bdc1
murphy-afk <bardarenoemi@gmail.com>
2025-12-04 16:09:20 +0100

    feat: inputs

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import './App.css'
 const initialFormData = {
   author: '',
   title: '',
-  content: ''
-  // to be added: public 
+  content: '',
+  status: ''
 }
 
 function App() {
@@ -21,11 +21,16 @@ function App() {
     setFormData(newPostData);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+
+  }
+
   return (
     <>
       <h1>Add a blog post</h1>
       <div className="container">
-        <form action="submit">
+        <form action="submit" onSubmit={handleSubmit}>
           <label htmlFor="author" className='form-label'>Author</label>
           <input
             type="text"
@@ -34,16 +39,44 @@ function App() {
             className='form-control'
             value={formData.name}
             onChange={updateForm} />
-          {/* <label htmlFor="title" className='form-label'>Title</label>
-          <input 
-          type="text" 
-          name='title' 
-          id='title' 
-          className='form-control' 
-          value={formData.title}/>
+          <label htmlFor="title" className='form-label'>Title</label>
+          <input
+            type="text"
+            name='title'
+            id='title'
+            className='form-control'
+            value={formData.name}
+            onChange={updateForm} />
           <label htmlFor="content" className='form-label'>Content</label>
-          <textarea name="content" id="content" className='form-control'></textarea>
-          <button className='btn btn-primary'>Submit</button> */}
+          <textarea
+            name="content"
+            id="content"
+            className='form-control'
+            value={formData.name}
+            onChange={updateForm}></textarea>
+          <div className='form-check'>
+            <label htmlFor="public" className='form-check-label'>Public</label>
+            <input
+              type="radio"
+              name="status"
+              id="public"
+              className='form-check-input' 
+              value='public'
+              checked={formData.status === 'public'}
+              onChange={updateForm}/>
+          </div>
+          <div className="form-check">
+            <label htmlFor="draft" className='form-check-label'>Draft</label>
+            <input
+              type="radio"
+              name="status"
+              id="draft"
+              className='form-check-input'
+              value='draft'
+              checked={formData.status === 'draft'}
+              onChange={updateForm} />
+          </div>
+          <button className='btn btn-primary'>Submit</button>
         </form>
       </div>
     </>
